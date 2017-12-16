0268b39f9287a2df412d5fbc048f21681d4bd012
Alex Lerner <alernerdev@gmail.com>
2017-12-16 18:44:54 -0500

    blah

--- js/app.bundle.js
+++ js/app.bundle.js
@@ -4,6 +4,8 @@ webpackJsonp([0],[
 
 var app = angular.module('dashboard', []);
 app.config(function($sceProvider) {
+    // for this exercise, I am turning this off since I am mostly interested in the
+    // bundling of code, not how the app works.
     $sceProvider.enabled(false);
 });
 
